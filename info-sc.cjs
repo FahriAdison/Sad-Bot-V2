d32e1d528ff59879c89e3dd902302e5e2a232ca0
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-10-21 14:25:46 +0000

    Salah Letak

--- info-sc.cjs
+++
@@ -1,9 +0,0 @@
-var handler = async(m, { conn, text, usedPrefix, command }) => {
-
-var str = `*https://github.com/FahriAdison/Sad-Bot-V2*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
-conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
-}
-handler.command = handler.help = ['sc']
-handler.tags = ['info']
-    
-module.exports = handler
