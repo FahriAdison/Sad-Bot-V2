2fad84267f3d193c2c63ce8c27fcf62b1969a63c
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-10-22 14:14:31 +0000

    📌Update

--- plugins/user-profile.cjs
+++ plugins/user-profile.cjs
@@ -1,96 +1,31 @@
-var PhoneNumber = require('awesome-phonenumber')
-var levelling = require('../lib/levelling.cjs')
-var handler = async (m, {
-	conn,
-	usedPrefix
-}) => {
-	var _p = profil
-	var who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.quoted ? m.quoted.sender : m.sender
-	try {
-		_p = await conn.profilePictureUrl(who, 'image')
-	} catch (e) {
+var PhoneNumber = require ('awesome-phonenumber')
+var moment = require ('moment-timezone')
 
-	} finally {
-		var about = (await conn.fetchStatus(who).catch(console.error) || {}).status || ''
-		var pp = /^https?:\/\//.test(_p) ? await getbuffer(_p) : _p
-		if (typeof db.data.users[who] == "undefined") {
-			db.data.users[who] = {
-				exp: 0,
-				limit: 10,
-				lastclaim: 0,
-				registered: false,
-				name: conn.getName(m.sender),
-				age: -1,
-				regTime: -1,
-				afk: -1,
-				afkReason: '',
-				banned: false,
-				level: 0,
-				role: 'Warrior V',
-				autolevelup: false
-			}
-		}
-		var {
-			name,
-			limit,
-			exp,
-			lastclaim,
-			registered,
-			regTime,
-			age,
-			level,
-			role,
-			banned,
-			pasangan,
-			premium
-		} = db.data.users[who]
-		var {
-			min,
-			xp,
-			max
-		} = levelling.xpRange(level, global.multiplier)
-		var username = conn.getName(who)
-		var math = max - xp
-		var jodoh
-		if (pasangan == "") {
-			jodoh = `Gak punya`
-		} else if (db.data.users[db.data.users[who].pasangan].pasangan != who) {
-			jodoh = `sedang menunggu jawaban dari @${pasangan.split('@')[0]}`
-		} else {
-			jodoh = `@${pasangan.split('@')[0]}`
-		}
-		var str = `
-Nama: ${username} ${registered ? '(' + name + ') ' : ''}(@${who.replace(/@.+/, '')})${about != 401 ? '\nInfo: ' + about : ''}
-Nomor: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-Link: https://wa.me/${who.split`@`[0]}${registered ? '\nUmur: ' + age : ''}
-XP: TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Siap untuk *${usedPrefix}levelup*` : `${math} XP lagi untuk levelup`}]
-Level: ${level}
-Role: *${role}*
-Limit: ${limit}
-Premium: ${premium ? 'Ya' : 'Tidak'}
-Terdaftar: ${registered ? 'Ya (' + new Date(regTime).toLocaleString() + ')' : 'Tidak'}${lastclaim > 0 ? '\nTerakhir Klaim: ' + new Date(lastclaim).toLocaleString() : ''}
-Pasangan: ${jodoh}
-`.trim()
-		var mentions = pasangan ? [who, pasangan] : [who]
-		conn.reply(m.chat, banned ? 'jiakh ke banned' : str, m, {
-			mentions,
-			contextInfo: {
-				externalAdReply: {
-					mediaType: 2,
-					description: 'anu',
-					title: await tiny('hai' + username),
-					mediaUrl: yt,
-					body: '𓃗𓅜',
-					thumbnail: pp,
-					sourceUrl: gc,
-					showAdAttribution: true, // false
-					//renderLargerThumbnail: true // false
-				}
-			}
-		})
-	}
+var handler = async (m, { conn, usedPrefix }) => {
+  var who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  var { status, setAt } = await conn.fetchStatus(who).catch(() => {
+          return {
+            status: "",
+            setAt: "",
+          };
+        });
+   var pp
+  try {
+    pp = await conn.profilePictureUrl(who, "image")
+  } catch (e) {
+    pp = "https://telegra.ph/file/e47d9ec693e5288ad9382.jpg"
+  } finally {
+    var username = conn.getName(who)
+    var str = `
+• Name: ${username}, \n• Tag: @${who.replace(/@.+/, '')}, ${status ? '\n• Bio: ' + status : ''}, \n• Set At Bio: ${(setAt && moment(setAt).format("DD MMMM YYYY")) || "Unknown"}, \n• Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')},\n• Link: https://wa.me/${who.split`@`[0]}`.trim()
+
+    var mentionedJid = [who]
+    conn.sendFile(m.chat, pp, 'pp.jpeg', str, m, false, { contextInfo: { mentionedJid }})
+  }
 }
-handler.help = ['profile *@user*']
-handler.tags = ['user']
-handler.command = /^profile$/i
+handler.help = ['profile [@user]']
+handler.tags = ['tools']
+handler.command = /^(profile)$/i
+handler.group = true
+
 module.exports = handler
