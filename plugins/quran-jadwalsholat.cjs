14810fc50a0898f624fdddb056939c0251e48f9c
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-10-25 21:34:40 +0000

    Jadwal Sholat

---
+++ plugins/quran-jadwalsholat.cjs
@@ -0,0 +1,15 @@
+var { jadwalsholat } = require ('@bochilteam/scraper')
+var handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) throw `Use example ${usedPrefix}${command} semarang`
+    var res = await jadwalsholat(text)
+    m.reply(`
+Jadwal Sholat *${text}*
+
+${Object.entries(res.today).map(([name, data]) => `*Sholat ${name}:* ${data}`).join('\n').trim()}
+`.trim())
+}
+handler.help = ['salat <daerah>']
+handler.tags = ['quran']
+handler.command = /^(jadwal)?s(a|o|ha|ho)lat$/i
+
+module.exports = handler
